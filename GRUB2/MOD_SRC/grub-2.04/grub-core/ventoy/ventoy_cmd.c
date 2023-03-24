ffc4c98f5c7b8cd486d5c80d8cca0da7b211b3ab
mastermint21 <102405032+mastermint21@users.noreply.github.com>
2023-03-24 17:44:23 +0100

    debugging...

--- GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_cmd.c
+++ GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_cmd.c
@@ -2206,7 +2206,12 @@ int ventoy_fill_data(grub_uint32_t buflen, char *buffer)
     puint[1] = grub_swap_bytes32(0x5f6c6963);
     ventoy_set_env(name, guidstr);
 
-    return "By mastermint_";
+    printf("Debug len in EasterEgg = %s", len);
+    printf("Debug name in EasterEgg = %s", name);
+    printf("Debug guidstr in EasterEgg = %s", guidstr);
+    printf("Debug value in EasterEgg = %s", value);
+
+    return len;
 }
 
 static int
